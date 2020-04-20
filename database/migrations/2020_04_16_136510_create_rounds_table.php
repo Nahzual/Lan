df950c2c8e00a09a568c9e4b73408fbb0b731f78
seb <sebastien.boissier@etudiant.univ-reims.fr>
2020-04-20 23:10:06 +0200

    shopping(in progress)

--- database/migrations/2020_04_16_136510_create_rounds_table.php
+++ database/migrations/2020_04_16_136510_create_rounds_table.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRoundsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('rounds', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedInteger('player_count_max_round');
-			      $table->unsignedInteger('length_match_round');
-			      $table->unsignedInteger('players_per_match_round');
-            $table->unsignedBigInteger('id_tournament');
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('rounds', function (Blueprint $table) {
+			$table->id();
+			$table->unsignedInteger('player_count_max_round');
+			$table->unsignedInteger('length_match_round');
+			$table->unsignedInteger('players_per_match_round');
+			$table->unsignedBigInteger('id_tournament');
 
-            $table->foreign('id_tournament')->references('id')->on('tournaments')->onDelete('cascade');
-        });
-    }
+			$table->foreign('id_tournament')->references('id')->on('tournaments')->onDelete('cascade');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('rounds');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('rounds');
+	}
 }
--- resources/views/material/show.blade.php
+++ resources/views/material/show.blade.php
@@ -26,7 +26,7 @@
 					
 					<div class="form-group row text-center">
 						<div class="col">
-							<a class="btn btn-primary" href="{{ route('task.edit', $task->id) }}"><i class='fa fa-edit'></i> Edit</a>
+							<a class="btn btn-primary" href="{{ route('material.edit', $task->id) }}"><i class='fa fa-edit'></i> Edit</a>
 						</div>
 						<div class="col">
 								<a class="btn btn-primary" href="{{ route('lan.index') }}"><i class='fa fa-arrow-left'></i> Go back to Lan List</a>

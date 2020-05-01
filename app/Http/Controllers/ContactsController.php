e1d52ed7545d2dc9a88f0b710080317c6bf84797
Nahzual <nahzual@gmail.com>
2020-05-01 14:17:33 +0200

    contact

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -46,29 +46,22 @@ class ContactsController extends Controller
 			'description' => ['required']
 		]);
 		
-		$messagebody  = 'MIME-Version: 1.0' . "\r\n";
-		$messagebody .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-		$messagebody .= 'De: ' . htmlentities($request->name) . "\r\n";
-		$objet = 'Message depuis le site Lan Creator';
-
 		$mail_cleaning = array("content-type","bcc:","to:","cc:","href");
-		$msg = str_replace($mail_cleaning,"",htmlentities($request->description));
-
-
-		$messagebody .= '<h1>'.$objet.'</h1>
-		<p><strong>' . htmlentities($request->lastname) .' '. htmlentities($request->name) .'</strong> a écrit :</p>
-		<p><strong>Message : </strong>' . $msg . '</p>';
 		
+		$sender = htmlentities($request->email);
 		
-
-		$sender = $request->email;
-		$object = $request->object;
+		$object = htmlentities($request->object);
+		$name = htmlentities($request->name);
+		$lastname = htmlentities($request->lastname);
+		$email = htmlentities($request->email);
+		$title = 'Message depuis le site Lan Creator';
+		$content = str_replace($mail_cleaning,"",htmlentities($request->description));
 		
-		Mail::send([], [], function ($message) use ($sender, $object, $messagebody) {
+		Mail::send('contact.mail', ['name' => $name, 'lastname' => $lastname, 'email' => $email, 'title' => $title, 'content' => $content], function ($message) use ($sender, $object) {
 		  $message->to('lancreator.noreply@gmail.com')
-				->from($sender)
-				->subject($object)
-				->setBody($messagebody); // assuming text/plain
+				->from($sender, 'Lan Creator')
+				->cc($sender)
+				->subject($object);
 		});
 		
 		return response()->json([

f9716e497e119f3f6dec80320c5a7b97e934fd91
gerf0001 <theo.gerfaux@etudiant.univ-reims.fr>
2020-05-05 17:31:03 +0200

    Dashboard tables : show/hide buttons

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -20,115 +20,118 @@
 									<form method="GET" action="{{ route('lan.create') }}">
 										@csrf
 										@method('GET')
-										<button type="submit" class="btn  btn-outline-dark shadow-sm float-right"><i class='fa fa-plus-square'></i> Create New Lan</button>
+										<button type="submit" class="btn  btn-outline-dark shadow-sm float-center"><i class='fa fa-plus-square'></i> Create New Lan</button>
 									</form>
+								</div>
+								<div class="col mt-1">
+									<button class="btn btn-outline-dark shadow-sm float-right" data-toggle="collapse" data-target="#my_lans" aria-expanded="false" aria-controls="my_lans">Show/hide</button>
 								</div>
 							</div>
 						</div>
 					</div>
-							<div class="table-responsive">
-								<table class="table card-table table-bordered">
-									<thead class="card-table text-center">
-									<th scope="col" >#</th>
-									<th scope="col" >Name</th>
-									<th scope="col" >Participants</th>
-									<th scope="col" >State</th>
-									<th scope="col" >View</th>
-									<th scope="col">Edit</th>
-									<th scope="col">Delete</th>
-									</thead>
+					<div class="table-responsive collapse" id="my_lans">
+						<table class="table card-table table-bordered">
+							<thead class="card-table text-center">
+								<th scope="col" >#</th>
+								<th scope="col" >Name</th>
+								<th scope="col" >Participants</th>
+								<th scope="col" >State</th>
+								<th scope="col" >View</th>
+								<th scope="col">Edit</th>
+								<th scope="col">Delete</th>
+							</thead>
 
-									<tbody>
-										@each('lan.my_lans',$admin_lans,'lan')
-									</tbody>
-								</table>
-							</div>
-
+							<tbody>
+								@each('lan.my_lans',$admin_lans,'lan')
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
+	<!-- Admin LANs -->
+	<div id="response-success-delete" class="container alert alert-success mt-2" style="display:none"></div>
+	<div id="response-error-delete" class="container alert alert-danger mt-2" style="display:none"></div>
 
-@yield('admin_section')
+	<!-- Helper LANs -->
+	<div class="container">
+	  <div class="row justify-content-center">
+	    <div class="col-md-10">
+	      <div class="card">
+	        <div class="card-header">
+	          <div class="row">
+	            <div class="col mt-2">
+	              <h3>LANs on which I am helper</h3>
+	            </div>
+							<div class="col mt-1">
+								<button class="btn btn-outline-dark shadow-sm float-right" data-toggle="collapse" data-target="#helper_lans" aria-expanded="false" aria-controls="helper_lans">Show/hide</button>
+							</div>
+	          </div>
+	        </div>
+	      </div>
 
+	      <div class="table-responsive collapse" id="helper_lans">
+	        <table class="table card-table table-bordered">
+	          <thead class="card-table text-center">
+	            <th scope="col" >#</th>
+	            <th scope="col" >Name</th>
+	            <th scope="col" >Participants</th>
+	            <th scope="col">State</th>
+	            <th scope="col">View</th>
+	          </thead>
 
+	          <tbody>
+	            @each('lan.helper_lans',$helper_lans,'lan')
+	          </tbody>
+	        </table>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 
-<!-- Admin LANs -->
-<div id="response-success-delete" class="container alert alert-success mt-2" style="display:none"></div>
-<div id="response-error-delete" class="container alert alert-danger mt-2" style="display:none"></div>
+	<br><br>
 
-<!-- Helper LANs -->
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-10">
-      <div class="card">
-        <div class="card-header">
-          <div class="row">
-            <div class="col mt-2">
-              <h3>LANs on which I am helper</h3>
-            </div>
-          </div>
-        </div>
-      </div>
+	<!-- Player LANs -->
 
-      <div class="table-responsive">
-        <table class="table card-table table-bordered">
-          <thead class="card-table text-center">
-            <th scope="col" >#</th>
-            <th scope="col" >Name</th>
-            <th scope="col" >Participants</th>
-            <th scope="col">State</th>
-            <th scope="col">View</th>
-          </thead>
+	<div id="response-success-player" class="container alert alert-success mt-2" style="display:none"></div>
+	<div id="response-error-player" class="container alert alert-danger mt-2" style="display:none"></div>
 
-          <tbody>
-            @each('lan.helper_lans',$helper_lans,'lan')
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</div>
+	<div class="container">
+	  <div class="row justify-content-center">
+	    <div class="col-md-10">
+	      <div class="card">
+	        <div class="card-header">
+	          <div class="row">
+	            <div class="col mt-2">
+	              <h3>LANs on which I am player</h3>
+	            </div>
+							<div class="col mt-1">
+								<button class="btn btn-outline-dark shadow-sm float-right" data-toggle="collapse" data-target="#player_lans" aria-expanded="false" aria-controls="player_lans">Show/hide</button>
+							</div>
+	          </div>
+	        </div>
+	      </div>
 
-<br><br>
+	      <div class="table-responsive collapse" id="player_lans">
+	        <table class="text-center table card-table table-bordered">
+	          <thead class="card-table text-center">
+	            <th scope="col" >#</th>
+	            <th scope="col" >Name</th>
+	            <th scope="col" >Participants</th>
+	            <th scope="col" >View</th>
+	            <th scope="col">Quit</th>
+	          </thead>
 
-<!-- Player LANs -->
-
-<div id="response-success-player" class="container alert alert-success mt-2" style="display:none"></div>
-<div id="response-error-player" class="container alert alert-danger mt-2" style="display:none"></div>
-
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-10">
-      <div class="card">
-        <div class="card-header">
-          <div class="row">
-            <div class="col mt-2">
-              <h3>LANs on which I am player</h3>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="table-responsive">
-        <table class="text-center table card-table table-bordered">
-          <thead class="card-table text-center">
-            <th scope="col" >#</th>
-            <th scope="col" >Name</th>
-            <th scope="col" >Participants</th>
-            <th scope="col" >View</th>
-            <th scope="col">Quit</th>
-          </thead>
-
-          <tbody>
-            @each('lan.player_lans',$player_lans,'lan')
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-</div>
+	          <tbody>
+	            @each('lan.player_lans',$player_lans,'lan')
+	          </tbody>
+	        </table>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 </div>
 @endsection
 

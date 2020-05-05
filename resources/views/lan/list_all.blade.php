9521b6ef4dce32562fc9d53868fa07d9efe40694
angom8 <angom8@outlook.fr>
2020-05-05 12:26:53 +0200

    (see last commit)

--- resources/views/lan/list_all.blade.php
+++ resources/views/lan/list_all.blade.php
@@ -50,37 +50,7 @@
     </div>
   </div>
   <div id="lanList">
-   <div class="table-responsive esp">
-	<table class="table card-table table-bordered  box_shadow ">
-		<thead class="text-center ">
-			<th scope="col">#</th>
-			<th scope="col" >Name</th>
-			<th scope="col" >Participants</th>
-			<th scope="col" >Date</th>
-			<th scope="col" ></th>
-			<th scope="col"></th>
-		</thead>
-
-		<tbody>
-			<tr>
-				@foreach($lans as $lan)
-				<?php $date = date_create($lan->opening_date); ?>
-				<th class="lead-text text-center">{{$lan->id}}</th>
-				<td class="lead-text text-center">{{$lan->name}}</td>
-				<td class="lead-text text-center">{{ $lan->real_user_count() }}/{{ $lan->max_num_registrants }}</td>
-				<td class="lead-text text-center">{{date_format($date, config("display.DATE_FORMAT"))}}</td>
-
-				<td class="text-center">
-					<a class="btn btn-success" href="{{ route('lan.show', $lan->id) }}"><i class='fa fa-eye'></i> View</a>
-				</td>
-				<td class="text-center">
-					<a class="btn btn-success" href="{{ route('lan.participate', $lan->id) }}"><i class="fa fa-sign-in"></i> Participate</a>
-				</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
-</div>
+	 @include('lan.all_lans_list',$lans)
   </div>
 </div>
 @endsection

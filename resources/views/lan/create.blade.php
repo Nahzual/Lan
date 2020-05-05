05a13db7642bfb511bd538293a7a31af9a40faa9
angom8 <angom8@outlook.fr>
2020-05-05 13:19:49 +0200

    Reworked Create a LAN

--- resources/views/lan/create.blade.php
+++ resources/views/lan/create.blade.php
@@ -1,70 +1,68 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-    	<div class="card">
-        <div class="card-header">
-					<h3 class="lead-title">Creating a new Lan</h3>
-				</div>
-				<div class="card-body">
+<div class="card">
+	<div class="card-header">
+		<h2>Create a LAN</h2>
+	</div>
+	<div class="card-body">
 
           <div id="response-success" class="container alert alert-success mt-2" style="display:none"></div>
           <div id="response-error" class="container alert alert-danger mt-2" style="display:none"></div>
 
-					{!! Form::open(['method' => 'put', 'id' => 'CreateNewLanForm']) !!}
-						<div class="bg-light">
-							<div class="form-group">
-								{!! Form::label('name', 'Name', ['class' => 'lead']) !!}
-								{!! Form::text('name', null, ['id'=>'name', 'class' => 'form-control']) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('max_num_registrants', 'Maximum numbers of registrants', ['class' => 'lead']) !!}
-								{!! Form::number('max_num_registrants', null, ['id'=>'max_num_registrants','min'=>'1', 'class' => 'form-control']) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('opening_date', 'Date', ['class' => 'lead']) !!}
-								{!! Form::date('opening_date', null, ['id'=>'opening_date','class' => 'form-control']) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('duration', 'Duration (in days)', ['class' => 'lead']) !!}
-								{!! Form::number('duration', null, ['id'=>'duration','min'=>'1','class' => 'form-control']) !!}
-							</div>
-							<div class="form-group">
-								{!! Form::label('budget', 'Budget (in €)', ['class' => 'lead']) !!}
-								{!! Form::number('budget', null, ['id'=>'budget','min'=>'0', 'class' => 'form-control']) !!}
-							</div>
-              <div class="form-group">
-                {!! Form::label('room_length', 'Room length (in meters)', ['class' => 'lead']) !!}
-                {!! Form::number('room_length', null, ['id'=>'room_length','min'=>'1', 'class' => 'form-control', 'onchange'=>'changeDimensions()']) !!}
-              </div>
-              <div class="form-group">
-                {!! Form::label('room_width', 'Room width (in meters)', ['class' => 'lead']) !!}
-                {!! Form::number('room_width', null, ['id'=>'room_width','min'=>'1', 'class' => 'form-control', 'onchange'=>'changeDimensions()']) !!}
-              </div>
-						</div>
-						<div class="bg-light">
-              {!! Form::label('location', 'Location', ['class' => 'lead']) !!}
-							<div id="location" class="input-group mb-1">
-								{!! Form::number('num_location', null, ['id'=>'num_location','min'=>'0', 'placeholder'=>'Street number','class' => 'form-control']) !!}
-                <span class="input-group-addon mr-2"></span>
-								{!! Form::text('name_street', null, ['id'=>'name_street','placeholder'=>'Street Name', 'class' => 'form-control']) !!}
-                <span class="input-group-addon mr-2"></span>
-                {!! Form::text('name_city', null, ['id'=>'name_city','placeholder'=>'City','class' => 'form-control']) !!}
-							</div>
-              <div class="input-group mb-5">
-                {!! Form::text('zip_city', null, ['id'=>'zip_city','placeholder'=>'ZIP Code','class' => 'form-control']) !!}
-                <span class="input-group-addon mr-2"></span>
-                {!! Form::text('name_department', null, ['id'=>'name_department','placeholder'=>'Region Name','class' => 'form-control']) !!}
-                <span class="input-group-addon mr-2"></span>
-                {!! Form::text('name_country', null, ['id'=>'name_country','placeholder'=>'Country Name', 'class' => 'form-control']) !!}
-							</div>
-						</div>
+			{!! Form::open(['method' => 'put', 'id' => 'CreateNewLanForm']) !!}
+				<div>
+					<div class="form-group">
+						{!! Form::label('name', 'Name', ['class' => 'display-6']) !!}
+						{!! Form::text('name', null, ['id'=>'name', 'class' => 'form-control']) !!}
+					</div>
+					<div class="form-group">
+						{!! Form::label('max_num_registrants', 'Maximum numbers of registrants', ['class' => 'display-6']) !!}
+						{!! Form::number('max_num_registrants', null, ['id'=>'max_num_registrants','min'=>'1', 'class' => 'form-control']) !!}
+					</div>
+					<div class="form-group">
+						{!! Form::label('opening_date', 'Date', ['class' => 'display-6']) !!}
+						{!! Form::date('opening_date', null, ['id'=>'opening_date','class' => 'form-control']) !!}
+					</div>
+					<div class="form-group">
+						{!! Form::label('duration', 'Duration (in days)', ['class' => 'display-6']) !!}
+						{!! Form::number('duration', null, ['id'=>'duration','min'=>'1','class' => 'form-control']) !!}
+					</div>
+					<div class="form-group">
+						{!! Form::label('budget', 'Budget (in €)', ['class' => 'display-6']) !!}
+						{!! Form::number('budget', null, ['id'=>'budget','min'=>'0', 'class' => 'form-control']) !!}
+					</div>
+              				<div class="form-group">
+              					 {!! Form::label('room_length', 'Room length (in meters)', ['class' => 'display-6']) !!}
+              				  	{!! Form::number('room_length', null, ['id'=>'room_length','min'=>'1', 'class' => 'form-control', 'onchange'=>'changeDimensions()']) !!}
+              				</div>
+              				<div class="form-group">
+                				{!! Form::label('room_width', 'Room width (in meters)', ['class' => 'display-6']) !!}
+                				{!! Form::number('room_width', null, ['id'=>'room_width','min'=>'1', 'class' => 'form-control', 'onchange'=>'changeDimensions()']) !!}
+              				</div>
+				</div>
+				<div>
+              				{!! Form::label('location', 'Location', ['class' => 'display-6']) !!}
+					<div id="location" class="input-group mb-1">
+						{!! Form::number('num_location', null, ['id'=>'num_location','min'=>'0', 'placeholder'=>'Street number','class' => 'form-control']) !!}
+                				<span class="input-group-addon mr-2"></span>
+						{!! Form::text('name_street', null, ['id'=>'name_street','placeholder'=>'Street Name', 'class' => 'form-control']) !!}
+                				<span class="input-group-addon mr-2"></span>
+               					 {!! Form::text('name_city', null, ['id'=>'name_city','placeholder'=>'City','class' => 'form-control']) !!}
+					</div>
+              				<div class="input-group mb-5">
+                				{!! Form::text('zip_city', null, ['id'=>'zip_city','placeholder'=>'ZIP Code','class' => 'form-control']) !!}
+                				<span class="input-group-addon mr-2"></span>
+                				{!! Form::text('name_department', null, ['id'=>'name_department','placeholder'=>'Region Name','class' => 'form-control']) !!}
+                				<span class="input-group-addon mr-2"></span>
+                				{!! Form::text('name_country', null, ['id'=>'name_country','placeholder'=>'Country Name', 'class' => 'form-control']) !!}
+					</div>
+				</div>
 
-						<hr/>
-						{!! Form::label('room_plan', 'Room plan :', ['class' => 'lead']) !!}
-						<p class="lead-text">Legend :</p>
+				<hr/>
+				{!! Form::label('room_plan', 'Room plan :', ['class' => 'display-6']) !!}
+					
+				<p class="lead-text">Legend :</p>
 						<table>
 							<tbody>
 								<tr>
@@ -93,26 +91,23 @@
 							</tbody>
 						</table>
 
-						<div id="plateau" class="form-group row text-center justify-content-center mt-5">
-							<div id="result">
+				<div id="plateau" class="form-group row text-center justify-content-center mt-5">
+					<div id="result">
 
-							</div>
-						</div>
+					</div>
+				</div>
 
-						<div class="form-group row text-center">
-							<div class="col">
-								<button type="submit" class="btn btn-primary" id="AddNewLanSubmit"><i class='fa fa-plus-square'></i> Add</button>
-							</div>
-							<div class="col">
-								<a class="btn btn-primary" href="{{ route('lan.index') }}"><i class='fa fa-arrow-left'></i> Go Back to Lan List</a>
-							</div>
-						</div>
-					{!! Form::close() !!}
+				<div class="form-group row text-center">
+					<div class="col">
+						<button type="submit" class="btn  btn-outline-success shadow-sm" id="AddNewLanSubmit"><i class='fa fa-plus-square'></i> Add</button>
+					</div>
+					<div class="col">
+						<a class="btn  btn-outline-info shadow-sm" href="{{ route('my_lans') }}"><i class='fa fa-arrow-left'></i> Go back to my LANs</a>
+					</div>
+				</div>
+				{!! Form::close() !!}
         </div>
       </div>
-    </div>
-  </div>
-</div>
 
 <script src=" http://code.jquery.com/jquery-3.3.1.min.js"
 					integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="

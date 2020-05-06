90a2d4061c9d082c7c2ff8df3079dff1f4f6d041
angom8 <angom8@outlook.fr>
2020-05-06 12:17:35 +0200

    (see previous commits)

--- resources/views/game/edit.blade.php
+++ resources/views/game/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="card-header">
 					<div class="row">
 						<div class="col mt-2">
-							<h3 class="lead-title">Editing Game : {{$game->name_game}}</h3>
+							<h3>Editing Game : {{$game->name_game}}</h3>
 						</div>
 					</div>
 				</div>
@@ -15,32 +15,32 @@
 
         <div class="card-body">
 				{!! Form::model($game, ['method' => 'put', 'onsubmit' => 'return sendRequest(event,'.$game->id.')']) !!}
-            <div class="bg-light">
+            <div >
               <div class="form-group">
-                {!! Form::label('name_game', 'Name', ['class' => 'lead']) !!}
+                {!! Form::label('name_game', 'Name', ['class' => 'display-6']) !!}
                 {!! Form::text('name_game', null, ['class' => 'form-control']) !!}
               </div>
               <div class="form-group">
-                {!! Form::label('desc_game', 'Description of the Game', ['class' => 'lead']) !!}
+                {!! Form::label('desc_game', 'Description of the Game', ['class' => 'display-6']) !!}
                 {!! Form::textarea('desc_game', null, ['class' => 'form-control','size'=>'30x5']) !!}
               </div>
               <div class="form-group">
-                {!! Form::label('release_date_game', 'Release Date', ['class' => 'lead']) !!}
+                {!! Form::label('release_date_game', 'Release Date', ['class' => 'display-6']) !!}
                 {!! Form::date('release_date_game', null, ['class' => 'form-control']) !!}
               </div>
               <div class="form-group">
-                {!! Form::label('cost_game', 'Price (in €) ', ['class' => 'lead']) !!}
+                {!! Form::label('cost_game', 'Price (in €) ', ['class' => 'display-6']) !!}
                 {!! Form::number('cost_game', null, ['min'=>'0', 'class' => 'form-control']) !!}
               </div>
               <div class="form-group">
-                {!! Form::label('is_multiplayer_game', 'Game Type : ', ['class' => 'lead']) !!}
+                {!! Form::label('is_multiplayer_game', 'Game Type : ', ['class' => 'display-6']) !!}
                 {!! Form::select('is_multiplayer_game', [config('game.SOLO') => '1 player', config('game.MULTI_LOCAL') => 'Local multiplayer', config('game.MULTI_ONL')=>'Online multiplayer'], $game->is_multiplayer_game, ['class' => 'form-control']) !!}
               </div>
             </div>
 
 						<div class="form-group row text-center">
 							<div class="col">
-								<button type="submit" class="btn btn-primary"><i class='fa fa-edit'></i> Update</button>
+								<button type="submit" class="btn btn-outline-warning"><i class='fa fa-edit'></i> Update</button>
 							</div>
 						</div>
 					{!! Form::close() !!}

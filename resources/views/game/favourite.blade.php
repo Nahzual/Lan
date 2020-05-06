93f119b4a439c07c73f324e20a2cd27100502177
angom8 <angom8@outlook.fr>
2020-05-06 12:24:19 +0200

    All game views are reworked

--- resources/views/game/favourite.blade.php
+++ resources/views/game/favourite.blade.php
@@ -1,22 +1,19 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
 
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-12">
       <div class="card">
         <div class="card-header">
           <div class="row">
             <div class="col mt-2">
-              <h3 class="lead-title">My favourite games</h3>
+              <h3>My precious</h3>
             </div>
 
             <div class="col mt-1">
               <form method="GET" action="{{ route('game.index') }}">
               @csrf
               @method('GET')
-                <button type="submit" class="btn btn-primary float-right"><i class='fa fa-plus-square'></i> Add a favourite game</button>
+                <button type="submit" class="btn btn-outline-dark float-right"><i class='fa fa-plus-square'></i> Find new games</button>
               </form>
             </div>
           </div>
@@ -27,8 +24,8 @@
 
         <div class="card-body">
           {!! Form::open(['method' => 'post','onsubmit'=>'return searchFavouriteGames(event)']) !!}
-            <div class="bg-light">
-              <h4 class='lead'>Game's name :</h4>
+            <div >
+              <h4 >What's gaming, doc ?</h4>
               <div class="form-group">
                 {!! Form::hidden('view_path', 'game.list') !!}
                 {!! Form::text('name_game', null, ['required'=>'', 'class' => 'form-control']) !!}
@@ -36,7 +33,7 @@
             </div>
             <div class="form-group row text-center">
               <div class="col">
-                <button type="submit" class="btn btn-primary"><i class='fa fa-search'></i> Search</button>
+                <button type="submit" class="btn btn-outline-info"><i class='fa fa-search'></i> Search</button>
               </div>
             </div>
           {!! Form::close() !!}
@@ -49,9 +46,6 @@
           @include('game.list')
         </div>
       </div>
-    </div>
-  </div>
-</div>
 @endsection
 
 @section('js_includes')

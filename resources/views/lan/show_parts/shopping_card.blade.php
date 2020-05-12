e4a4949e0c07e3282a594e6849832f3bd6bf832e
Nahzual <nahzual@gmail.com>
2020-05-12 11:51:46 +0200

    shopping

--- resources/views/lan/show_parts/shopping_card.blade.php
+++ resources/views/lan/show_parts/shopping_card.blade.php
@@ -7,7 +7,7 @@
 								@if ($userIsLanAdmin)
 								<div class="col">
 									<button class="btn btn-outline-dark shadow-sm float-right ml-2" data-toggle="collapse" data-target="#lan_shopping" aria-expanded="false" aria-controls="lan_shopping">Show/hide</button>
-									<a class="btn btn-success shadow-sm float-right ml-2" href="{{ route('lan.add_shopping', $lan->id) }}"><i class='fa fa-plus'></i></a>
+									<a class="btn btn-success shadow-sm float-right ml-2" href="{{ route('shopping.create', $lan->id) }}"><i class='fa fa-plus'></i></a>
 									<a class="btn btn-outline-primary shadow-sm float-right" href="{{ route('lan.add_game', $lan->id) }}"><i class='fa fa-plus'></i> All</a>
 								</div>
 								@endif

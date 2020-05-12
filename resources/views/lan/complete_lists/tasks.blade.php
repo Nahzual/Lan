29a77b8cb501556d1e28411652015400a6fb96f5
angom8 <angom8@outlook.fr>
2020-05-12 15:58:03 +0200

    Added the complete Task list for a LAN

---
+++ resources/views/lan/complete_lists/tasks.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col mt-2">
+              <h3>All the tasks for the LAN {{ $nlan }}</h3>
+            </div>
+         
+		<div class="col mt-1">
+              <form method="GET" action="{{ route('task.create', $id) }}">
+              @csrf
+              @method('GET')
+                <button type="submit" class="btn btn-outline-success float-right"><i class='fa fa-plus-square'></i> Add a Task</button>
+              </form>
+            </div>
+	 </div>
+        </div>
+
+        <div id="response-success" class="alert alert-success mt-2" style="display:none"></div>
+        <div id="response-error" class="alert alert-danger mt-2" style="display:none"></div>
+
+        <div class="card-body">
+          
+
+<div class="table-responsive">
+									<table class="text-center table card-table table-bordered">
+										<thead class="card-table text-center">
+										<th scope="col" >#</th>
+										<th scope="col" >Name</th>
+										<th scope="col" >Actions</th>
+									</thead>
+
+									<tbody>
+									@if(count($tasks)==0)
+										<tr>
+											<td colspan="5"><h3 class="text-center">No tasks to show</h3></td>
+										</tr>
+									@endif
+									@foreach($tasks as $task)
+										<tr id="row-task-lan-{{$task->id}}">
+											<th scope="row">{{$task->id}}</th>
+											<td scope="col">{!!$task->name_task!!}</td>
+											<td scope="col" class=" text-center">
+		           					<div class="btn-group">
+													{!! Form::open(['method'=>'get','url'=>route('task.show',[$lan->id,$task->id])]) !!}
+														<button type="submit" class="btn btn-success mr-2"><i class='fa fa-eye'></i> View</button>
+													{{ Form::close() }}
+													@if ($userIsLanAdmin)
+													{!! Form::open(['method'=>'get','url'=>route('task.edit',[$lan->id,$task->id])]) !!}
+														<button type="submit" class="btn btn-warning mr-2"><i class='fa fa-edit'></i> Edit</button>
+													{{ Form::close() }}
+													{!! Form::open(['method'=>'get','url'=>route('task.assign',[$lan->id,$task->id])]) !!}
+														<button type="submit" class="btn btn-primary mr-2"><i class='fa fa-user-plus'></i> Assign</button>
+													{{ Form::close() }}
+													{!! Form::open(['method' => 'delete','url'=>'', 'onsubmit'=>'return deleteTask(event, '.$lan->id.', '.$task->id.')']) !!}
+														<button type="submit" class="btn btn-danger"><i class='fa fa-trash'></i> Delete</button>
+													{!! Form::close() !!}
+													@endif
+												</div>
+											</td>
+										</tr>
+									@endforeach
+									</tbody>
+								</table>
+							</div>
+
+
+
+
+
+
+
+		<nav aria-label="page navigation">
+		  <ul class="pagination justify-content-end">
+			<li class="page-item">
+		      <a class="btn btn-info" href="{{ url('/lan/'.$id) }}" tabindex="-2">Return to the LAN</a>
+
+		    <li class="page-item">
+		      <a class="btn btn-secondary" href="{{ url('/lan/'.$id.'/tasks/') }}" tabindex="-2">First</a>
+		    </li>
+		    <li class="page-item @if($previous == 0) disabled @endif">
+		      <a class="btn btn-outline-info" href="{{ $previous!=0 ?  url('/lan/'.$id.'/tasks/'.($previous)) : '#' }}" tabindex="-1">Back</a>
+		    </li>
+		    <li class="page-item"><a class="btn btn-outline-dark" href="{{ url('/lan/'.$id.'/tasks/'.($page)) }}">{{ $page }}</a></li>
+		    @if(($page+1)<=$max) <li class="page-item"><a class="btn btn-outline-dark" href="{{ url('/lan/'.$id.'/tasks/'.($page+1)) }}">{{ ($page+1) }}</a></li>@endif
+		    @if(($page+2)<=$max)<li class="page-item"><a class="btn btn-outline-dark" href="{{ url('/lan/'.$id.'/tasks/'.($page+2)) }}">{{ ($page+2) }}</a></li>@endif
+		    <li class="page-item @if($next) @else disabled @endif">
+		      <a class="btn btn-outline-info" href="{{ $next ? url('/lan/'.$id.'/tasks/'.($next)) : '#' }}">Next</a>
+		    </li>
+		    <li class="page-item">
+		      <a class="btn btn-secondary" href="{{ url('/lan/'.$id.'/tasks/'.$max) }}">Last</a>
+		    </li>
+		  </ul>
+</nav>
+
+        </div>
+
+      </div>
+@endsection
+
+@section('js_includes')
+<script type="text/javascript" src="/js/ajax/game/ajax.js"></script>
+@endsection
+
+@section('css_includes')
+<link href="{{ asset('css/game/game.css') }}" rel="stylesheet">
+<link href="{{ asset('css/table-style.css') }}" rel="stylesheet">
+@endsection
--- resources/views/lan/show_parts/tasks_card.blade.php
+++ resources/views/lan/show_parts/tasks_card.blade.php
@@ -8,7 +8,7 @@
 									<button class="btn btn-outline-dark shadow-sm float-right ml-2" data-toggle="collapse" data-target="#lan_tasks" aria-expanded="false" aria-controls="lan_tasks">Show/hide</button>
 									@if ($userIsLanAdminOrHelper)
 									<a class="btn btn-success shadow-sm float-right ml-2" href="{{ route('task.create', $lan->id) }}"><i class='fa fa-plus'></i></a>@endif
-									<a class="btn btn-outline-primary shadow-sm float-right" href="{{ route('lan.add_game', $lan->id) }}"><i class='fa fa-list'></i> All</a>
+									<a class="btn btn-outline-primary shadow-sm float-right" href="{{ route('lan.task_list', $lan->id) }}"><i class='fa fa-list'></i> All</a>
 
 								</div>
 							</div>

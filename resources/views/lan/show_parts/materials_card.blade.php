e87ae21fe6339292124dc4bef57dfe9d9dbae70f
angom8 <angom8@outlook.fr>
2020-05-12 16:06:41 +0200

    Added the complete Material list for a LAN

--- resources/views/lan/show_parts/materials_card.blade.php
+++ resources/views/lan/show_parts/materials_card.blade.php
@@ -8,7 +8,7 @@
 								<div class="col">
 									<button class="btn btn-outline-dark shadow-sm float-right ml-2" data-toggle="collapse" data-target="#lan_materials" aria-expanded="false" aria-controls="lan_materials">Show/hide</button>
 									<a class="btn btn-success shadow-sm float-right ml-2" href="{{ route('lan.add_material', $lan->id) }}"><i class='fa fa-plus'></i></a>
-									<a class="btn btn-outline-primary shadow-sm float-right" href="{{ route('lan.add_game', $lan->id) }}"><i class='fa fa-list'></i> All</a>
+									<a class="btn btn-outline-primary shadow-sm float-right" href="{{ route('lan.material_list', $lan->id) }}"><i class='fa fa-list'></i> All</a>
 								</div>
 								@endif
 							</div>

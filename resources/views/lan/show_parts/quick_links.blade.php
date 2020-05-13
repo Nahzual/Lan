dd98d5ec87d9bce7a1e010359fd26266a0583cb7
angom8 <angom8@outlook.fr>
2020-05-13 15:04:48 +0200

    Added the quick-links card

--- resources/views/lan/show_parts/quick_links.blade.php
+++ resources/views/lan/show_parts/quick_links.blade.php
@@ -4,15 +4,50 @@
 							<h4 class="mt-2">Quick-Links</h4>
 						</div>
 					</div>
-					<div class="card-body">
-					<p>Liste complète des joueurs</p>
-					<p>Liste des admins</p>
-					<p>Liste des helpers</p>
-					<p>Liste des activités</p>
-					<p>Liste des jeux</p>
-					<p>Liste des taches</p>
-					<p>Liste des tournois</p>
-					<p>Liste du materiel</p>
-					<p>Shopping list</p>
-					</div>	
+					<div class="card-body h-100">
+
+									<div class="row justify-content-center">
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-dark shadow-sm  lan-btn-quick  w-100" href="{{ route('lan.activity_list', $lan->id) }}"><i class='fa fa-puzzle-piece'></i> Activities</a>
+										</div>
+										<div class="col-md-4 text-center  space-remove">
+											<a class="btn btn-dark shadow-sm  lan-btn-quick  w-100" href="{{ route('lan.game_list', $lan->id) }}"><i class='fa fa-gamepad'></i> Games</a>
+										</div>
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-dark shadow-sm  lan-btn-quick   w-100" href="{{ route('lan.tour_list', $lan->id) }}"><i class='fa fa-trophy'></i> Tournaments</a>
+										</div>
+									</div>
+
+									@if($userIsLanAdminOrHelper)
+									<div class="row justify-content-center">
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-or shadow-sm  lan-btn-quick  w-100" href="{{ route('lan.task_list', $lan->id) }}"><i class='fa fa-tasks'></i> Tasks</a>
+										</div>
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-or shadow-sm  lan-btn-quick  w-100" href="{{ route('lan.material_list', $lan->id) }}"><i class='fa fa-archive'></i> Materials</a>
+										</div>
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-or shadow-sm  lan-btn-quick  w-100" href="{{ route('lan.shopping_list', $lan->id) }}"><i class='fa fa-tags'></i> Shoppings</a>
+										</div>
+									</div>
+									<div class="row justify-content-center">
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-purple shadow-sm  lan-btn-quick  w-100" href="{{ route('lan.user_list', $lan->id) }}"><i class='fa fa-user'></i> Players</a>
+										</div>
+										@if($userIsLanAdmin)
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-purple shadow-sm lan-btn-quick   w-100" href="{{ route('lan.admin_list', $lan->id) }}"><i class='fa fa-user'></i> Admins</a>
+										</div>
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-purple shadow-sm lan-btn-quick  w-100" href="{{ route('lan.helper_list', $lan->id) }}"><i class='fa fa-user'></i> Helpers</a>
+										</div>
+										@endif
+									</div>
+									@endif
+									<div class="row justify-content-center">
+										<div class="col-md-4 text-center space-remove">
+											<a class="btn btn-info shadow-sm lan-btn-quick  w-100" href="{{ route('lan.guest_show', $lan->id) }}"><i class='fa fa-eye'></i> Public View</a>
+										</div>
+									</div>
+						</div>	
 				</div>

2f474fb3c70dede681f6d7c5318ac51c0459f122
gerf0001 <theo.gerfaux@etudiant.univ-reims.fr>
2020-05-14 22:54:04 +0200

    Issue#20 fixed

--- resources/views/material/create.blade.php
+++ resources/views/material/create.blade.php
@@ -14,20 +14,18 @@ Creating new Material
 <div id="response-error" class="alert alert-danger mt-2" style="display:none"></div>
 
 {!! Form::open(['method' => 'post', 'onsubmit'=>'return sendRequest(event);']) !!}
-	<div class="bg-light">
-		<div class="form-group">
-			{!! Form::label('name_material', 'Name') !!}
-			{!! Form::text('name_material', null, ['class' => 'form-control']) !!}
-		</div>
-		<div class="form-group">
-			{!! Form::label('desc_material', 'Description of the material') !!}
-			{!! Form::text('desc_material', null, ['class' => 'form-control']) !!}
-		</div>
-		<div class="form-group">
-			{!! Form::label('category_material', 'Category of the material') !!}
-			<small class="d-block">You can enter several categories by spacing them out with a delimiter of your choice (comma, white space...)</small>
-			{!! Form::text('category_material', null, ['class' => 'form-control']) !!}
-		</div>
+	<div class="form-group">
+		{!! Form::label('name_material', 'Name') !!}
+		{!! Form::text('name_material', null, ['class' => 'form-control']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('desc_material', 'Description of the material') !!}
+		{!! Form::text('desc_material', null, ['class' => 'form-control']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('category_material', 'Category of the material') !!}
+		<small class="d-block">You can enter several categories by spacing them out with a delimiter of your choice (comma, white space...)</small>
+		{!! Form::text('category_material', null, ['class' => 'form-control']) !!}
 	</div>
 	<div class="form-group row text-center">
 		<div class="col">

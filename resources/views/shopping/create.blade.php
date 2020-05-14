501a9746a4ccb14d5505d8124a8ad177fd8ec321
Nahzual <nahzual@gmail.com>
2020-05-14 11:22:23 +0200

    shopping debug

--- resources/views/shopping/create.blade.php
+++ resources/views/shopping/create.blade.php
@@ -31,7 +31,7 @@ Creating new Shopping
 		</div>
 
 		<div class="col">
-			<a class="btn btn-primary" href="{{ route('lan.index') }}"><i class='fa fa-arrow-left'></i> Go Back to Lan List</a>
+			<a class="btn btn-outline-info shadow-sm" href="{{ route('lan.show', $lan) }}"><i class='fa fa-arrow-left'></i> Go Back to Lan</a>
 		</div>
 	</div>
 {!! Form::close() !!}

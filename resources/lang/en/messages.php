fd4f90529f93395d34d4c26baebacd8ac70bb06f
nini <emilieetienne51@gmail.com>
2020-05-20 18:50:09 +0200

    translation and tournament correctif

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -211,12 +211,12 @@ return [
 	'error_404_message' => 'The page you attempted to reach is not registered.',
 	'error_500' => '500 error',
 	'error_500_message' => 'The server encountered a fatal error. Please, contact the website\'s admin',
-	
+
 	// Ports
 	'add_port_game_lan' => 'Adding port to game ":game" for LAN :lan',
 	'add_port_game' => 'Adding port to game ":game"',
 	'port' => 'Port :',
-	
+
 	// games
 	'release_date' => 'Release date',
 	'price' => 'Price (in €) ',
@@ -233,7 +233,7 @@ return [
 	'create_new_game' => 'Create a new game',
 	'add_game' => 'Add game',
 	'game' => 'Game',
-	
+
 	// team
 	'add_players' => 'Add players',
 	'add_users' => 'Add users',
@@ -250,9 +250,9 @@ return [
 	'team_members'=> 'Team players',
 	'all_players_team' => 'All the players of the team ',
 	'cannot_create_team_solo' => 'You can\'t create teams for this tournament, as the match mode of this tournament is solo.',
-	
+
 	// Account deletion
-	
+
 	'account_deletion' => 'Account deletion',
 	'confirm_wish_delete' => 'Do you really want to delete your account ?',
 	'deletion_consequence' => 'You will no longer be able to log in to your account after this operation.',
@@ -262,7 +262,7 @@ return [
 	'proceed' => 'Proceed ?',
 	'disable_account' => 'Disable your account',
 	'delete_account' => 'Delete your account',
-	
+
 	// Tasks
 	'task_page' =>'Task page',
 	'viewing_task' =>'Viewing task ',
@@ -277,7 +277,7 @@ return [
 	'adding_helper_task' => 'Adding helper to Task',
 	'editing_task' => 'Editing Task',
 	'assign_to_helper' => 'Assign to an helper',
-	
+
 	// material
 	'materials' => 'Materials',
 	'edit_material' => 'Editing Material',
@@ -288,7 +288,7 @@ return [
 	'no_materials' => 'No materials to show',
 	'add_material' => 'Add material',
 	'edit_quantity' => 'Edit quantity',
-	
+
 	// Shopping
 	'' => 'The Shopping List for the LAN',
 	'to_buy' => 'To buy',
@@ -301,7 +301,7 @@ return [
 	'create_new_shopping' => 'Creating new Shopping',
 	'add_shopping' => 'Add shopping',
 	'viewing_shopping_lan' => 'Viewing shopping list element of',
-	
+
 	// tournament
 	'add_to_tournament' => 'Add to tournament',
 	'add_player_tournament' => 'Add players to tournament',
@@ -321,19 +321,19 @@ return [
 	'edit_tournament' => 'Editing Tournament',
 	'tournament_state' => 'State of tournament :',
 	'tournament_page' => 'Tournament_page',
-	
+
 	'teams_for_tournament' => 'All teams for the tournament',
 	'tournament_teams' => 'Tournament\'s teams',
-	'add_team' => 'Add a team'.
+	'add_team' => 'Add a team',
 	'about' => 'About',
 	'tree' => 'Tree',
 
-	
+
 	// echo __('messages.welcome', ['name' => 'dayle']);
 	//'welcome' => 'Welcome, :NAME', // Welcome, DAYLE
 	//'goodbye' => 'Goodbye, :Name', // Goodbye, Dayle
 	//{{ __('messages.add_port_game', ['game' => $game->name_game, 'lan' => $lan->name]) }}
-	
+
 	// {{ __('messages.') }}
 
 	'quick_link' => 'Quick-Links',
